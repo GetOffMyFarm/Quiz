67246d3ed53cb9764f49f042344aa493ae9acb5b
Joe Freeman <joefreeman@joes-imac.myfiosgateway.com>
2021-03-16 21:02:49 -0400

    Adding new Javaascript from branch

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,20 @@
-var timer = document.querySelector("#timer");
-var highScore = document.querySelector("#hscore");
+var timer = document.getElementById("timer");
+var highScore = document.getElementById("hscore");
+var start = document.getElementById('start-button');
+var question1 = document.getElementById('question1');
+var question2 = document.getElementById('question2');
+var question3 = document.getElementById('question3');
+var question4 = document.getElementById('question4');
+var question5 = document.getElementById('question5');
+var questions = [question1, question2, question3, question4, question5];
+var startMenu = document.getElementById('card1');
+var answer1 = document.querySelector('input[name="json"]:checked');
+var answer2 = document.querySelector('input[name="log"]:checked');
+var answer3 = document.querySelector('input[name="reset"]:checked');
+var answer4 = document.querySelector('input[name="flexbox"]:checked');
+var answer5 = document.querySelector('input[name="api"]:checked');
+answers = [answer1, answer2, answer3, answer4, answer5];
+var lastCard = document.getElementById('high-score-page');
 
 function countDown() {
     var newTime = 60;
@@ -14,10 +29,22 @@ function countDown() {
             clearInterval(timerInt);
         }
     }, 1000)
-}
-function checkAnswer(ans) {
+};
 
-    if (ans.value == 'right') {
 
+
+start.addEventListener("click", function() {
+    countDown();
+    startMenu.style.display = "none";
+    question1.style.display = "flex";
+    for (var i=0; i<questions.length; i++) {
+        if (answers[i].value === "right") {
+            questions[i].style.display = "none";
+            questions[i+1].style.display = "flex";
+        }
+        else {
+            newTime -= 10;
+        }
     }
-}
+
+})
