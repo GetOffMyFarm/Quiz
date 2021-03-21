6f11b71ee22c00d1d9c4be9ff9cd0372042ef4d2
Joseph Freeman <freemanjj1992@gmail.com>
2021-03-20 21:25:35 -0400

    final version of application, all logic working and updated

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,6 @@
+//added all neccessary API variables as well as arrays to generate questions
 var timer = document.getElementById("timer");
-var highScore = document.getElementById("hscore");
+var highScore = document.getElementById("hScore");
 var start = document.getElementById('start-button');
 var startMenu = document.getElementById('card1');
 var questionCard = document.getElementById('quiz-card');
@@ -16,40 +17,133 @@ var wrongAnswers = [['Juicey Story On Networktelevision', 'Jolly Santa Offering 
 var lastCard = document.getElementById('high-score-page');
 var turnIndex = 0;
 var radioButton = questionCard.querySelectorAll('button');
-var wrongValues = document.querySelectorAll('button[value = "wrong"]')
-var rightValue = document.querySelectorAll('button[value = "right"]')
-console.log(radioButton);
-console.log(wrongValues);
-
-
-for (var i = 0; i <radioButton.length; i++) {
-    radioButton[i].addEventListener("click", function(event) {
-        console.log("change", event.target.value);
-    });
-};
-
-function rightAnswer() {
-    
+var fillInQuestions = document.getElementById('questions');
+var wrongValues = document.querySelectorAll('button[value = "wrong"]');
+var rightValue = document.querySelectorAll('button[value = "right"]');
+var showScore = document.getElementById("score");
+var theScore = 0;
+var highScoreSubmit = document.getElementById('submit-button');
+var highScoreFinal = localStorage.getItem('highScore');
+var highScoreInit = localStorage.getItem('highScoreInitials');
+var gameOver = document.getElementById('game-over-page');
+var storedInit = document.getElementById('highScore').value;
+var restart = document.getElementById('restart-game');
+//if there is a high score in local storage, it will be displayed
+if (highScoreFinal == null) {
+    highScore.textContent = 0;
 }
-
+else {
+    highScore.textContent = highScoreFinal + "" + highScoreInit;
+}
+//function to be called later that randomly sets the value of one of the buttons as "right" so we can place a right answer there
+function rightAnswer() {
+    buttonValue = radioButton.item(Math.floor(Math.random() * 4));
+    buttonValue.value = 'right';
+};
+//sets all button values to wrong so that one can randomly be set to "right"
+function resetAnswer() {
+    for (var i = 0; i < radioButton.length; i++) {
+        newButtonValue = radioButton.item(i);
+        newButtonValue.value = 'wrong';
+    }
+};
+//function that starts a countdown that ends when the timer reaches zero or when the quiz ends (the turn index conditional)
 function countDown() {
-    var newTime = 60;
+    newTime = 60;
     
     var timerInt = setInterval(function() {
-        if (newTime > 0) {
+        if (newTime >= 0) {
             timer.textContent = newTime;
             newTime--;
          }
+        else if (turnIndex == questions.length) {
+            timer.textContent = 0;
+            clearInterval(timerInt);
+        }
         else {
-            timer.textContent = '';
+            timer.textContent = 0;
             clearInterval(timerInt);
         }
     }, 1000)
 };
-
+//function to be called later that stops the question card and displays the end of game cards.
+function endGame() {
+    if (theScore > highScoreFinal) {
+        questionCard.style.display = "none";
+        lastCard.style.display = "flex";
+        turnIndex = 0;
+    }
+    else {
+        questionCard.style.display = "none";
+        gameOver.style.display = "flex";
+    }
+}
+//function that generates the questions on the screen
+function setUpQuestions() {
+    resetAnswer();
+    rightAnswer();
+    wrongValues = document.querySelectorAll('button[value = "wrong"]')
+    rightValue = document.querySelectorAll('button[value = "right"]')
+    fillInQuestions.textContent = questions[turnIndex];
+    rightValue.item(0).textContent = rightAnswers[turnIndex];
+    wrongValues.item(0).textContent = wrongAnswers[turnIndex][0];
+    wrongValues.item(1).textContent = wrongAnswers[turnIndex][1];
+    wrongValues.item(2).textContent = wrongAnswers[turnIndex][2];
+};
+//for loop that checks if an answer is right or wrong based on clicking a button, also ends the game when turn index is over
+for (var k = 0; k <radioButton.length; k++) {
+    radioButton.item(k).addEventListener("click", function(event) {
+        if (event.target.value == 'wrong') {
+            newTime -= 10;
+            if (newTime <= 0) {
+                showScore.textContent = theScore;
+                endGame();
+                newTime = 0;
+                timer.textContent = 0;
+            }
+        }
+        else {
+            theScore +=1;
+            showScore.textContent = theScore;
+            turnIndex += 1;
+            if (turnIndex == questions.length) {
+                theScore += newTime;
+                showScore.textContent = theScore;
+                endGame();
+                newTime = 0;
+                timer.textContent = 0;
+            }
+            else{
+            setUpQuestions();
+            }
+        }
+    });
+};
+//click event that starts the game
 start.addEventListener("click", function() {
     countDown();
     startMenu.style.display = "none";
     questionCard.style.display = "flex";
+    setUpQuestions();
 });
-
+//submits initials and score to local storage then displays it at the top of the screen, as well as shows the last element to restart
+highScoreSubmit.addEventListener("click", function() {
+    storedInit = document.getElementById('highScore').value;
+    localStorage.setItem("highScore", theScore)
+    localStorage.setItem("highScoreInitials", storedInit);
+    highScoreFinal = localStorage.getItem('highScore');
+    highScoreInit = localStorage.getItem('highScoreInitials');
+    highScore.textContent = highScoreFinal + "" + highScoreInit;
+    lastCard.style.display = "none";
+    gameOver.style.display = "flex";
+});
+//restarts the game and resets all variables that were added on to or subtracted from
+restart.addEventListener("click", function() {
+    gameOver.style.display = "none";
+    turnIndex = 0;
+    newTime = 0;
+    timer.textContent = 0;
+    theScore = 0;
+    showScore.textContent = 0;
+    startMenu.style.display = "flex";
+})
---
+++ README.md
@@ -0,0 +1,28 @@
+# 04 Web APIs: Code Quiz
+
+
+## User Story
+
+```
+AS A coding boot camp student
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers
+```
+
+## Final Behavior
+
+```
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and my score
+```
+
+A launched version of this application can be found at: https://getoffmyfarm.github.io/Beginners_Coding_Quiz/
